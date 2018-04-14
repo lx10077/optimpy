3b81721941f46aae93228528f111396bf3dea317
Lix <lixiang10077@pku.edu.cn>
2018-04-14 22:03:15 +0800

    add models and trainer

---
+++ trainer.py
@@ -0,0 +1,69 @@
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+import os
+import torch
+import shutil
+
+class Trainer(object):
+    def __init__(self, dataset):
+        self.best_acc = 0
+
+    def test(testloader, model, criterion, epoch, use_cuda):
+        global best_acc
+
+        batch_time = AverageMeter()
+        data_time = AverageMeter()
+        losses = AverageMeter()
+        top1 = AverageMeter()
+        top5 = AverageMeter()
+
+        # switch to evaluate mode
+        model.eval()
+
+        end = time.time()
+        bar = Bar('Processing', max=len(testloader))
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            # measure data loading time
+            data_time.update(time.time() - end)
+
+            if use_cuda:
+                inputs, targets = inputs.cuda(), targets.cuda()
+            inputs, targets = torch.autograd.Variable(inputs, volatile=True), torch.autograd.Variable(targets)
+
+            # compute output
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            # measure accuracy and record loss
+            prec1, prec5 = accuracy(outputs.data, targets.data, topk=(1, 5))
+            losses.update(loss.data[0], inputs.size(0))
+            top1.update(prec1[0], inputs.size(0))
+            top5.update(prec5[0], inputs.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            # plot progress
+            bar.suffix = '({batch}/{size}) Data: {data:.3f}s | Batch: {bt:.3f}s | Total: {total:} | ETA: {eta:} | Loss: {loss:.4f} | top1: {top1: .4f} | top5: {top5: .4f}'.format(
+                batch=batch_idx + 1,
+                size=len(testloader),
+                data=data_time.avg,
+                bt=batch_time.avg,
+                total=bar.elapsed_td,
+                eta=bar.eta_td,
+                loss=losses.avg,
+                top1=top1.avg,
+                top5=top5.avg,
+            )
+            bar.next()
+        bar.finish()
+        return (losses.avg, top1.avg)
+
+    def save_checkpoint(self, state, is_best, checkpoint='checkpoint', filename='checkpoint.pth.tar'):
+        filepath = os.path.join(checkpoint, filename)
+        torch.save(state, filepath)
+        if is_best:
+            shutil.copyfile(filepath, os.path.join(checkpoint, 'model_best.pth.tar'))
